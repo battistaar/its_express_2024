959bb92c549f2c4dd648409e02b6c5ae96fc0cb1
Enrico Battistella <enrico.battistella@gmail.com>
2024-02-14 13:59:01 +0100

    data generation

---
+++ product-generator.ts
@@ -0,0 +1,33 @@
+// {
+//   id: string, database.mongodbObjectId
+//   name: string, commerce.productName
+//   description: string, commerce.productDescription
+//   netPrice: number, commerce.price
+//   weight: number, number.int
+//   discount: number number.float
+// }
+
+import { fakerIT as faker } from '@faker-js/faker';
+import { writeFileSync } from 'fs';
+
+function generateRandomProduct() {
+  return {
+    id: faker.database.mongodbObjectId(),
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    netPrice: parseFloat(faker.commerce.price({max: 10000})),
+    weight: faker.number.int({min: 50, max: 2000}),
+    discount: faker.number.float({min: 0, max: 1, fractionDigits: 2})
+  }
+}
+
+function generateProducts(num: number) {
+  const products: any[] = [];
+  for(let i = 0; i < num; i++) {
+    products.push(generateRandomProduct());
+  }
+  // const products = Array.from({length: num}).map(() => generateRandomProduct());
+  writeFileSync('./products.json', JSON.stringify(products, null, 2), {encoding: 'utf-8'});
+}
+
+generateProducts(200);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { Request, Response, NextFunction } from 'express';
+import PRODUCTS from '../products.json';
 
 const app = express();
 
 app.use(cors());
 app.use(morgan('tiny'));
 
-const cart = [
+interface CartItem {
+  name: string;
+  netPrice: number;
+  weight: number;
+  discount: number;
+  quantity: number;
+}
+
+const cart: CartItem[] = [
 	{
 		name: 'ssd',
 		netPrice: 95,
@@ -66,19 +76,35 @@ const cart = [
 	}
 ];
 
-app.get('/', (req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('my middleware');
+
+  next();
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('my second middleware');
+
+  next();
+})
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Content-Type', 'text/plain');
   res.status(200);
   res.send('hello world');
 });
 
-app.get('/cart-items', (req, res) => {
+app.get('/cart-items', (req: Request, res: Response) => {
+  console.log('handler');
+
   res.json(cart);
 });
 
-app.listen(3000);
+app.get('/products', (req: Request, res: Response) => {
+  res.json(PRODUCTS);
+})
 
-// http.createServer((req, res) => {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World');
-// }).listen(3000);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
